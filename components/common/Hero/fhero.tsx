5637b4c28d214e3fe50df5048db52b161a8765b9
“GodwinEbikwo” <godwin.a.ebikwo@gmail.com>
2022-03-18 08:00:22 +0000

    changes

--- components/common/Hero/fhero.tsx
+++ components/common/Hero/fhero.tsx
@@ -1,85 +1,10 @@
 //@ts ignore
 import Div100vh from 'react-div-100vh'
-import styled from 'styled-components'
 import Link from 'next/link'
 import { m } from 'framer-motion'
 import { revealIn, wrapperVariants } from '@config/transitions'
 import Image from 'next/image'
-
-const Root = styled.div`
-  position: relative;
-`
-
-const Left = styled(m.div)`
-  display: flex;
-  width: 50vw;
-  border-bottom: 1px solid var(--border-color);
-  border-right: 1px solid var(--border-color);
-
-  &.has-border-left {
-    border-left: 1px solid var(--border-color);
-  }
-
-  .imgContainer {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    overflow: clip;
-
-    @media (max-width: 1023px) {
-      height: 100%;
-    }
-  }
-
-  @media (max-width: 1023px) {
-    width: 100vw;
-    height: 100vw;
-    height: 108vw;
-    border-bottom: none;
-    border-right: none;
-  }
-`
-
-const Right = styled(m.div)`
-  display: flex;
-  width: 50vw;
-  border-bottom: 1px solid var(--border-color);
-  border-right: none;
-
-  @media (max-width: 1023px) {
-    justify-content: center;
-    text-align: center;
-    width: 100vw;
-    border-bottom: 1px solid var(--border-color);
-    border-right: none;
-  }
-
-  .rightInner {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    padding: 15px 15px 45px;
-
-    @media (max-width: 1023px) {
-      width: 100%;
-      padding-bottom: var(--spacer);
-      align-items: flex-start;
-    }
-
-    .a {
-      text-transform: uppercase;
-      margin-bottom: 0.2em;
-      font-family: var(--font2);
-    }
-
-    .b {
-      margin-bottom: 0.2em;
-      letter-spacing: var(--ls-md);
-      text-transform: uppercase;
-    }
-  }
-`
+import s from './f.module.css'
 
 const v = {
   initial: { opacity: 0 },
@@ -121,12 +46,17 @@ export const Final = ({
   location,
 }: Props) => {
   return (
-    <Root>
+    <div className={s.root}>
       {variant === 'default' && (
         <Div100vh className="flex flex-wrap">
-          <Left initial="initial" whileInView="enter" variants={v}>
+          <m.div
+            initial="initial"
+            whileInView="enter"
+            variants={v}
+            className={s.left}
+          >
             <m.div
-              className="imgContainer"
+              className={s.imgContainer}
               data-scroll
               variants={wrapperVariants}
             >
@@ -140,19 +70,24 @@ export const Final = ({
                 className="himg"
               />
             </m.div>
-          </Left>
+          </m.div>
 
-          <Right initial="initial" whileInView="enter" variants={v}>
-            <div className="rightInner">
+          <m.div
+            initial="initial"
+            whileInView="enter"
+            variants={v}
+            className={s.right}
+          >
+            <div className={s.rightInner}>
               <Wrapper>
                 <m.div variants={revealIn}>
-                  <span className="a">{caption}</span>
+                  <span className={s.a}>{caption}</span>
                 </m.div>
               </Wrapper>
 
               <Wrapper>
                 <m.div variants={revealIn}>
-                  <h2 className="b">{title}</h2>
+                  <h2 className={s.b}>{title}</h2>
                 </m.div>
               </Wrapper>
 
@@ -164,23 +99,28 @@ export const Final = ({
                 </m.div>
               </Wrapper>
             </div>
-          </Right>
+          </m.div>
         </Div100vh>
       )}
 
       {variant === 'two' && (
         <Div100vh className="flex flex-wrap hide-for-mobile">
-          <Right initial="initial" whileInView="enter" variants={v}>
-            <div className="rightInner">
+          <m.div
+            initial="initial"
+            whileInView="enter"
+            variants={v}
+            className={s.right}
+          >
+            <div className={s.rightInner}>
               <Wrapper>
                 <m.div variants={revealIn}>
-                  <span className="a">Accesories</span>
+                  <span className={s.a}>Accesories</span>
                 </m.div>
               </Wrapper>
 
               <Wrapper>
                 <m.div variants={revealIn}>
-                  <h2 className="b">For Groceries</h2>
+                  <h2 className={s.b}>For Groceries</h2>
                 </m.div>
               </Wrapper>
 
@@ -192,22 +132,22 @@ export const Final = ({
                 </m.div>
               </Wrapper>
             </div>
-          </Right>
+          </m.div>
 
-          <Left
+          <m.div
             initial="initial"
             whileInView="enter"
             variants={v}
-            className="has-border-left "
+            className={`${s.left} ${s.has_border_left}`}
           >
             <m.div
-              className="imgContainer"
               data-scroll
               variants={wrapperVariants}
+              className={s.imgContainer}
             >
               <Image
                 priority={true}
-                alt="shoe"
+                alt="bag"
                 src="https://res.cloudinary.com/godwinebikwo/image/upload/v1647365809/forma_bag1_atbfgx.jpg"
                 layout="fill"
                 objectFit="cover"
@@ -215,9 +155,9 @@ export const Final = ({
                 className="himg"
               />
             </m.div>
-          </Left>
+          </m.div>
         </Div100vh>
       )}
-    </Root>
+    </div>
   )
 }
