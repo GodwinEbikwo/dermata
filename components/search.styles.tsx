ffd316659728185787e54456ce03dcffd9e92213
“GodwinEbikwo” <godwin.a.ebikwo@gmail.com>
2022-03-18 14:48:23 +0000

    wilding

--- components/search.styles.tsx
+++ components/search.styles.tsx
@@ -16,6 +16,8 @@ export const SearchContainer = styled.div`
     padding-top: var(--spacer-half);
     font-family: var(--font2);
     border-top: 1px solid var(--border-color);
+    border-left: 1px solid var(--border-color);
+    border-right: 1px solid var(--border-color);
 
     @media (min-width: 768px) {
       border-top: none;
